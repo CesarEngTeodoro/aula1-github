d174429f720fd957bb7884aeddca376bce67e518
CesarEngTeodoro <cesar.eng.teodoro@gmail.com>
2019-05-11 11:01:34 -0300

    Acrescentada mensagem de bom dia

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,9 @@
 package application;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.util.Calendar;
-import java.util.Date;
-
 public class Program {
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args)  {
 		
-		
-		
+		System.out.println("Bom dia!");	
 
 	}
 }
